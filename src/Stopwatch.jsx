57a8257cef935eb40c0aee55650d0b840bf6c203
dfrancese <davefrancese@gmail.com>
2018-06-30 17:57:37 -0600

    add leadingZero func

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -5,21 +5,44 @@ class Stopwatch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: 0
+      time: 30,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
     };
   }
+
+  leadingZero(num) {
+    return num < 10 ? "0" + num : num;
+  }
+
   render() {
     return (
       <div className="stopwatch">
         <h4>Stopwatch</h4>
         <Form inline>
           <FormControl
-            className="stopwatch-input"
-            placeholder="time on the clock"
+            onChange={event => this.setState({ hours: event.target.value })}
+            className="stopwatch-hours"
+            placeholder="hours"
+          />
+          <FormControl
+            onChange={event => this.setState({ minutes: event.target.value })}
+            className="stopwatch-minutes"
+            placeholder="minutes"
+          />
+          <FormControl
+            onChange={event => this.setState({ seconds: event.target.value })}
+            className="stopwatch-seconds"
+            placeholder="seconds"
           />
           <Button>Go!</Button>
         </Form>
-        <div>{this.state.time}</div>
+        <div>
+          {this.leadingZero(this.state.hours)}:{this.leadingZero(
+            this.state.minutes
+          )}:{this.leadingZero(this.state.seconds)}
+        </div>
       </div>
     );
   }
